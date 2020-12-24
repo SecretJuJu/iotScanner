f870a9ee6d4b71fec163214eacb0ed71554c6ff9
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2020-12-24 10:26:33 +0900

    fix upload

--- app.py
+++ app.py
@@ -121,9 +121,9 @@ def exploit_upload():
                         sql = "insert into Exploit (name,company,version,productName,args,exploitMovement,path) values (?,?,?,?,?,?,?);"
                         last_id = dbCur.execute(sql, [name,company,version,productName,args_json,exploitMovement,path]).lastrowid
                         dbCon.commit()
+                        return "<script>location.href=/exploit/detail?id="+last_id+"</script>"
                     except Exception as e :
                         return "<script>alert('not a zip file');history.back()</script>"
-                return "<script>location.href=/exploit/detail?id="+last_id+"</script>"
             else :
                 return "<script>alert('not a zip file');history.back()</script>"
         except Exception as e :
